9ec1491135738df911cbefce82a6f6ce9ec2c9ed
Aditya Kotre <89739833+AdityaKotre@users.noreply.github.com>
2023-03-19 17:58:23 +0530

    Update RPC_game_Aditya.py

--- RPC_game_Aditya.py
+++ RPC_game_Aditya.py
@@ -1,25 +1,32 @@
-import random
+#Import random library
+import random 
 
 user_wins =0
 computer_wins =0
 
-options = ["rock","paper","scissors"]
+options = ["rock","paper","scissor"]     # Creating 'options' list of elements 
 
+
+""" Using while loop asking user for his pick
+    If user want to quit, then he need to type 'Q'
+    And if user input is not present in option then
+    it will ask untill the correct pick
+"""
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    user_input = input("Type Rock/Paper/Scissor or Q to quit: ").lower()
     if user_input == "q":
         break
 
     if user_input not in options:
         continue
 
-    random_number = random.randint(0,2)
-    # rock:0 paper:1 scissors: 2
-    computer_pick = options[random_number]
+    random_number = random.randint(0,2)              #Random number creation by random function
+    # rock:0 paper:1 scissors: 2            
+    computer_pick = options[random_number]           # Computer pick
     print("Computer picked", computer_pick + ".")
 
-    if user_input == 'rock' and computer_pick == 'scissors':
+    if user_input == 'rock' and computer_pick == 'scissor':
         print("You Won!")
         user_wins += 1
     
@@ -27,7 +34,7 @@ while True:
         print("You Won!")
         user_wins += 1
     
-    elif user_input == 'scissors' and computer_pick == 'paper':
+    elif user_input == 'scissor' and computer_pick == 'paper':
         print("You Won!")
         user_wins += 1
 
